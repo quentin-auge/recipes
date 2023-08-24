b211a0b1d42a07a3b54dd028e06814d3d8c1322d
Quentin Auge <quentin.auge@gmail.com>
2023-08-24 16:10:42 +0200

    Prevent accidental HTTP calls in tests

--- backend/tests/scrapper/conftest.py
+++ backend/tests/scrapper/conftest.py
@@ -4,3 +4,10 @@ import pytest
 @pytest.fixture(scope="session")
 def data_folder() -> Path:
     return Path(__file__).parent / "data"
+
+@pytest.fixture(scope="session", autouse=True)
+def forbid_http_requests(session_mocker):
+    def urlopen_mock(self, method, url, **kwargs):
+        raise RuntimeError(f"Unexpected HTTP call: {method} {url}")
+
+    session_mocker.patch("urllib3.connectionpool.HTTPConnectionPool.urlopen", urlopen_mock)
